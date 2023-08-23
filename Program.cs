6020361a1791bb00c817619883378ed13411b0b5
Khagan95 <85906243+Khagan95@users.noreply.github.com>
2023-08-23 13:03:23 +0300

    Whıle_Foreach
    Whıle_Foreach

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Foreach
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+
+			int sayac = 1;
+            while (sayac <= 5)
+            {
+                Console.WriteLine(sayac);
+                sayac++;
+            }
+            
+			Console.Write("***** Foreach *****");
+			string[] arabalar = { "BMV", "Ford", "Toyota", "Nissan" };
+			foreach (var araba in arabalar)
+			{
+				Console.WriteLine(araba);
+			}
+			
+		}
+	}
+}
